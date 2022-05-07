120002ed4fa14b7f5f7971b3173cf5579333434e
Hannah Graves <hannahmgraves@gmail.com>
2022-05-07 13:18:02 -0700

    updating css issues with gh pages

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -14,9 +14,11 @@ const About = () => {
 				</div>
 				
 				<div className='align-rt col about-text'>
-					I am a full-stack developer with a passion for creating high-end user experiences. I thrive when challenged and am always willing to continue learning. 
+					I am a full-stack developer with a passion for creating high-end user experiences. I thrive when challenged and fully believe that you can never stop learning.  
 					<br /><br />
 					I have a certificate in Software Engineering Immersive from General Assembly, as well as a Bachelor of Science in Biology from Oregon State University. 
+					<br /><br />
+					Feel free to reach out, I would love to connect!
 				</div>
 			</div>	
 		</div>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css';
 const Footer = () => {
 	return (
 		<footer>
-			<aside>This portfolio was made with React</aside>
+			<aside>This portfolio was made with React by Hannah Graves. 2022</aside>
 		</footer>
 	);
 };
