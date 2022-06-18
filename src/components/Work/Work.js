4f40bc97e0e541f68b9e36a6dab5fba1e70f69eb
Hannah Graves <hannahmgraves@gmail.com>
2022-06-17 17:34:34 -0700

    updating css with grid

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -11,151 +11,125 @@ const Work = () => {
 			<h2>WORK</h2>
 
 			{/* TackTrunk */}
-			<div className="row">
-			<div className="cols">
-					<img className="work-img img-left" src={tackpage} alt="tackpage..." />
-					<br /><br /><br /><br />
+			<div className='work-container-1'>
+				<h3 className='title-1 title'>TackTrunk</h3>
+				<p className='about-1'>TackTrunk is a full CRUD application built with the busy horse barn in mind. Tack Trunk allows users to log in and add/categorize their personal tack as well as schedule lessons with the barn's trainers.</p>
+				<p className='built-1'>
+					Built with:
+					<i className="devicon-react-original small-icon"></i>
+					<i className="devicon-javascript-plain opposite small-icon"></i>
+					<i className="devicon-nodejs-plain small-icon"></i>
+					<i className="devicon-mongodb-plain small-icon"></i>
+					<i className="devicon-express-original small-icon"></i>
+					<i className="devicon-css3-plain small-icon"></i>
+					<i className="devicon-html5-plain small-icon"></i>
+				</p>
+				<img className="work-img img-1" src={tackpage} alt="tackpage..." />
+				<div className='btn-block links-1'>
 					<a href="https://tacktrunk.herokuapp.com/" style={{ textDecoration: 'none', cursor: 'pointer', color: 'blue' }}>
-							View on Heroku
+						View on Heroku
 					</a>
-					<br /><br /><br />
 					<a href="https://github.com/hmgraves/TackTrunk" style={{ textDecoration: 'none', cursor: 'pointer', color: 'blue' }}>
 						View on GitHub
 					</a>
 				</div>
-				<div className="cols align-rt">
-				<h3 className='title'>TackTrunk</h3>
-					<p>TackTrunk is a full CRUD application built with the busy horse barn in mind. Tack Trunk allows users to log in and add/categorize their personal tack as well as schedule lessons with the barn's trainers.</p>
-					<p>
-						Built with:
-						<i className="devicon-react-original small-icon"></i>
-						<i className="devicon-javascript-plain opposite small-icon"></i>
-						<i className="devicon-nodejs-plain small-icon"></i>
-						<i className="devicon-mongodb-plain small-icon"></i>
-						<i className="devicon-express-original small-icon"></i>
-						<i className="devicon-css3-plain small-icon"></i>
-						<i className="devicon-html5-plain small-icon"></i>
-					</p>
-				</div>
 			</div>
+			<hr />
 
 
 			{/* Vines */}
-			<div className="row">
-			<hr />
-				<div className="cols">
-					<h3 className='title'>Vines</h3>
-					<p>Vines is a full CRUD application built for project two at General Assembly. Vines is a personalized database for you to store, inventory, and manage your houseplants.</p>
-					<p>
-						Built with:
-						<i className="devicon-javascript-plain opposite small-icon"></i>
-						<i className="devicon-nodejs-plain small-icon"></i>
-						<i className="devicon-mongodb-plain small-icon"></i>
-						<i className="devicon-express-original small-icon"></i>
-						<i className="devicon-css3-plain small-icon"></i>
-						<i className="devicon-html5-plain small-icon"></i>
-					</p>
+			<div className='work-container-2'>
+				<h3 className='title title-2'>Vines</h3>
+				<p className='about-2'>Vines is a full CRUD application built for project two at General Assembly. Vines is a personalized database for you to store, inventory, and manage your houseplants.</p>
+				<p className='built-2'>
+					Built with:
+					<i className="devicon-javascript-plain opposite small-icon"></i>
+					<i className="devicon-nodejs-plain small-icon"></i>
+					<i className="devicon-mongodb-plain small-icon"></i>
+					<i className="devicon-express-original small-icon"></i>
+					<i className="devicon-css3-plain small-icon"></i>
+					<i className="devicon-html5-plain small-icon"></i>
+				</p>
+				<img className="work-img img-2" src={vines} alt="vines..." />
+				<div className='btn-block links-2'>
+					<a href="https://vines-app.herokuapp.com/" style={{ textDecoration: 'none', cursor: 'pointer', color: 'blue' }}>
+						View on Heroku
+					</a>
+					<a href="https://github.com/hmgraves/Plant-app" style={{ textDecoration: 'none', cursor: 'pointer', color: 'blue' }}>
+						View on GitHub
+					</a>
 				</div>
-				<div className="cols button">
-					<img className="work-img img-right" src={vines} alt="vines..." />
-					<div className='btn-block'>
-						<br /><br /><br /><br />
-						<a href="https://vines-app.herokuapp.com/" style={{ textDecoration: 'none', cursor: 'pointer', color: 'blue' }}>
-							View on Heroku
-						</a>
-						<br /><br />
-						<a href="https://github.com/hmgraves/Plant-app" style={{ textDecoration: 'none', cursor: 'pointer', color: 'blue' }}>
-							View on GitHub
-						</a>
-					</div>
-
-				</div>
-				<hr />
 			</div>
+			<hr />
 
-			{/* CarCollector */}
-			<div className="row">
-				<div className="cols">
-					<img className="work-img img-left" src={cars} alt="cars..." />
-					<br /><br /><br /><br />
+			{/* Car Collector */}
+			<div className='work-container-1'>
+				<h3 className='title-1 title'>CarCollector</h3>
+				<p className='about-1'>CarCollector is a full-stack CRUD app using Django and Python. CarCollector is designed for you to keep track of your cars, including maintenance and any additional options you might upgrade your car with.</p>
+				<p className='built-1'>
+					Built with:
+					<i className="devicon-python-plain small-icon"></i>
+					<i className="devicon-postgresql-plain small-icon"></i>
+					<i className="devicon-django-plain small-icon"></i>
+					<i className="devicon-css3-plain small-icon"></i>
+					<i className="devicon-html5-plain small-icon"></i>
+				</p>
+				<img className="work-img img-1" src={cars} alt="cars..." />
+				<div className='btn-block links-1'>
+
 					<a href="https://github.com/hmgraves/carcollector" style={{ textDecoration: 'none', cursor: 'pointer', color: 'blue' }}>
 						View on GitHub
 					</a>
 				</div>
-				<div className="cols align-rt">
-					<h3 className='title'>CarCollector</h3>
-					<p>CarCollector is a full-stack CRUD app using Django and Python. CarCollector is designed for you to keep track of your cars, including maintenance and any additional options you might upgrade your car with.</p>
-					<p>
-						Built with:
-						<i className="devicon-python-plain small-icon"></i>
-						<i className="devicon-postgresql-plain small-icon"></i>
-						<i className="devicon-django-plain small-icon"></i>
-						<i className="devicon-css3-plain small-icon"></i>
-						<i className="devicon-html5-plain small-icon"></i>
-					</p>
-				</div>
 			</div>
+			<hr />
 
 			{/* Travel */}
-			<div className="row">
-			<hr />
-				<div className="cols">
-					<h3 className='title'>Travel</h3>
-					<p>This travel app, built as a group project, allows users to choose a destination and view attractions at that destination. Also included is Weatherbug API, so users can see real-time weather at their chosen.</p>
-					<p>
-						Built with:
-						<i className="devicon-python-plain small-icon"></i>
-						<i className="devicon-postgresql-plain small-icon"></i>
-						<i className="devicon-django-plain small-icon"></i>
-						<i className="devicon-css3-plain small-icon"></i>
-						<i className="devicon-html5-plain small-icon"></i>
-					</p>
+			<div className='work-container-2'>
+				<h3 className='title title-2'>Travel</h3>
+				<p className='about-2'>This travel app, built as a group project, allows users to choose a destination and view attractions at that destination. Also included is Weatherbug API, so users can see real-time weather at their chosen..</p>
+				<p className='built-2'>
+					Built with:
+					<i className="devicon-python-plain small-icon"></i>
+					<i className="devicon-postgresql-plain small-icon"></i>
+					<i className="devicon-django-plain small-icon"></i>
+					<i className="devicon-css3-plain small-icon"></i>
+					<i className="devicon-html5-plain small-icon"></i>
+				</p>
+				<img className="work-img img-2" src={travel} alt="mines..." />
+				<div className='btn-block links-2'>
+					<a href="https://sei-travel-app.herokuapp.com/" style={{ textDecoration: 'none', cursor: 'pointer', color: 'blue' }}>
+						View on Heroku
+					</a>
+					<a href="https://github.com/hmgraves/travel-app" style={{ textDecoration: 'none', cursor: 'pointer', color: 'blue' }}>
+						View on GitHub
+					</a>
 				</div>
-				<div className="cols">
-					<br /><br />
-					<img className="work-img img-right" src={travel} alt="mines..." />
-					<div className='btn-block'>
-						<br /><br /><br /><br />
-						<a href="https://sei-travel-app.herokuapp.com/" style={{ textDecoration: 'none', cursor: 'pointer', color: 'blue' }}>
-							View on Heroku
-						</a>
-						<br /><br />
-						<a href="https://github.com/hmgraves/travel-app" style={{ textDecoration: 'none', cursor: 'pointer', color: 'blue' }}>
-							View on GitHub
-						</a>
-						
-					</div>
-					
-				</div>
-				<br />
-				<hr />
 			</div>
+			<hr />
 
 			{/* Minesweeper */}
-			<div className="row">
-				<div className="cols">
-					<img className="work-img img-left" src={mines} alt="mines..." />
-					<br /><br /><br /><br />
+			<div className='work-container-1'>
+				<h3 className='title-1 title'>Minesweeper</h3>
+				<p className='about-1'>his is the classic game, Minesweeper. This was the second game ever built by Hannah. The most difficult part of building this game, was using recursion!</p>
+				<p className='built-1'>
+					Built with:
+					<i className="devicon-javascript-plain opposite small-icon"></i>
+					<i className="devicon-css3-plain small-icon"></i>
+					<i className="devicon-html5-plain small-icon"></i>
+					<i className="devicon-jquery-plain small-icon"></i>
+				</p>
+				<img className="work-img img-1" src={mines} alt="mines..." />
+				<div className='btn-block links-1'>
 					<a href="https://hmgraves.github.io/Minesweeper/" style={{ textDecoration: 'none', cursor: 'pointer', color: 'blue' }}>
 						View on GitHub Pages
 					</a>
-					<br /><br /><br />
 					<a href="https://github.com/hmgraves/Minesweeper" style={{ textDecoration: 'none', cursor: 'pointer', color: 'blue', }}>
 						View on GitHub
 					</a>
 				</div>
-				<div className="cols align-rt">
-					<h3 className='title'>Minesweeper</h3>
-					<p>This is the classic game, Minesweeper. This was the second game ever built by Hannah. The most difficult part of building this game, was using recursion!</p>
-					<p>
-						Built with:
-						<i className="devicon-javascript-plain opposite small-icon"></i>
-						<i className="devicon-css3-plain small-icon"></i>
-						<i className="devicon-html5-plain small-icon"></i>
-						<i className="devicon-jquery-plain small-icon"></i>
-					</p>
-				</div>
 			</div>
+
 		</div>
 	);
 
