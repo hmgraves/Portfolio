65845fe9bf849ab834b2f49676bf3c209e044f40
Hannah G <hannahmgraves@gmail.com>
2024-10-29 14:33:56 -0700

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,10 +7,10 @@ const About = () => {
 			<h2 className="row-about">ABOUT</h2>
 			<img className="about-photo" src={Hannah} alt="author..." tabindex="-1"/>
 			<div className="about-text">
-				I am a full-stack developer with a passion for creating high-end user experiences. I thrive when challenged and fully believe that you can never stop learning.
+				I am a Software Engineer at Scisco Genetics with a passion for creating high-end user experiences. I have a strong foundation in front end development with popular technologies such as TypeScript, Angular, React, HTML, SCSS, and Electron, as well as experience with popular CSS frameworks like Material-UI, Material, and Ionic. 
 				<br />
 				<br />
-				I successfully completed the Software Engineering Immersive program from General Assembly, as well as a Bachelor of Science in Biology from Oregon State University.
+				I successfully completed the Software Engineering Immersive program from General Assembly, as well as a Bachelor of Science in Biology from Oregon State University, and am currently working on a Master of Science in Microbiology &amp; Cell Science
 				<br />
 				<br />
 				I have previous experience in the Healthcare industry working first as a medical assistant and then as a manager leading a team of nurses and medical assistants.
